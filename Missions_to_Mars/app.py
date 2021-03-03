c2bceebe282ea019385d4f5dea440a4315aa8910
Evan Mobley <emmobley63@gmail.com>
2021-03-02 20:38:31 -0600

    ,found news_p

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -3,7 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def scrape():
+def ():
+
+
+@app.route("/scrape")
+def import_scrape()
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
